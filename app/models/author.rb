21514b4798893d030e6201bc1a64c7e67068121e
Badi-Haki Martin <jojackblack@gmail.com>
2022-09-07 11:26:21 -0400

    commit tree c52aae5296181c156790bfa2c5e84dba4877e9f8

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,7 @@
 class Author < ApplicationRecord
+    # authors have name
+    # authors  names are unique
+    # phone #10 digits
+    validates :name, {presence: true, uniqueness: true }
+    validates :phone_number, length: {is: 10}
 end
